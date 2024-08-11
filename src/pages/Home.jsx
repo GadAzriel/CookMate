874533a3503e4c19911ee14bce4aec88bc5af378
Gad Azriel <gadazriel7@gmail.com>
2024-08-11 05:07:23 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Home = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
